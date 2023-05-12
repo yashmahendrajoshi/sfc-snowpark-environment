fafcfae16e61b1d9aa335f44e52c38c6428c6b50
Enrique Plata <enrique.plata@snowflake.com>
2023-05-12 05:42:25 +0200

    Adding sckit-learn (#5)
    Co-authored-by: eplata <domfp13@me.com>

---
+++ server/src/03_scikit_learn/config.py
@@ -0,0 +1,13 @@
+snowflake_conn_prop = {
+   "account": "",
+   "user": "",
+   "password": "",
+   "role": "ACCOUNTADMIN",
+   "database": "SCIKIT_LEARN",
+   "schema": "PUBLIC",
+   "warehouse": "LEARNINGSNOWPARKVW",
+}
+
+# Snowflake Account Identifiers
+# https://docs.snowflake.com/en/user-guide/admin-account-identifier.html#account-identifier-formats-by-cloud-platform-and-region
+#  
